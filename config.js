23a5ab46af245c282761346da2a766813f393085
InVTWorld <86712079+InVTWorld@users.noreply.github.com>
2021-06-30 02:41:13 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Thằng chó XL!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Lo ỉa đi
+    Học học cc`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Làm cc tao đi 😙',
+    desc: 'Thi xong qua nhà mày chơi nhen nhok ',
+    btnYes: 'oki em sẽ luôn là đàn em của anh Phat',
+    btnNo: 'Đéo nhé',
+    question:'Gửi đàn em thân yêu',
+    btnReply: 'Thấy tao như thế nào',
+    reply: 'đẹp trai khoai to khỏi phải lo ế',
+    mess: 'Tao biết mà 🥰. Yêu mày nhiều nhiều 😘😘',
+    messDesc: 'Lo học đi chơi cc.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
